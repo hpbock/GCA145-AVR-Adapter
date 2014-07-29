6c9989b28adc11e222dbe3a390b04e9f807cc889
Hans-Peter Bock <hpbock@avaapgh.de>
2014-07-29 21:39:25 +0200

    added send_2dec()

--- main.c
+++ main.c
@@ -6,10 +6,14 @@ uint8_t characters[] = {0b11000000, 0b11111001, 0b10100100, 0b10110000,
                         0b10011001, 0b10010010, 0b10000010, 0b11111000,
                         0b10000000, 0b10010000};
 
+void send_2dec(uint8_t value) {
+	send_i2c(64, characters[value%10]);
+	send_i2c(66, characters[(value/10)%10]);
+}
+
 int main (void) {
 
-	send_i2c(64, characters[0]);
-	send_i2c(66, characters[0]);
+	send_2dec(123);
 
 	return 0;
 }
